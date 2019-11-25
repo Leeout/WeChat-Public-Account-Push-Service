a78086e7f7fc860a7804c012e036583356c079b1
李杰04 <lijie04@qutoutiao.net>
2019-11-25 17:41:36 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,13 @@
 # 部署前提，需要docker-compose.yml文件存在！
-docker-compose up --build
+cmd=$(docker-compose ps | grep web)
+if [[ ! ${cmd} ]]; then
+  docker-compose up -d
+  echo "服务部署成功！"
+else
+  docker-compose stop
+  echo "服务已停止！"
+  sleep 3
+  echo "服务开始重新部署..."
+  docker-compose up --build
+  echo "服务部署成功！"
+fi
